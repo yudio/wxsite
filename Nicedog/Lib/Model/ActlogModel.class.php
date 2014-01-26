5953a51a93cb851dbe9ba1f89a00f0cc3dfe72ee
samon yu <yudio@hotmail.com>
2014-01-26 18:00:39 +0800

    wxsite
    nice partner

---
+++ Nicedog/Lib/Model/ActlogModel.class.php
@@ -0,0 +1,45 @@
+<?php 
+class ActlogModel extends Model {
+    //自动完成
+	/*protected $_auto = array (
+		array('password','md5',1,'function'),	//新增时
+		array('last_login_time','time',1,'function'), 	//新增时
+		array('last_login_ip','127.0.0.1'), 	//新增时
+		array('last_location','新建用户'), 	//新增时
+	);
+
+	//自动验证
+	protected $_validate=array(
+		array('username','require','用户名称必须！',1,'',3),
+		array('username','','用户名称已经存在！',1,'unique',3), // 新增修改时候验证username字段是否唯一
+	);*/
+
+	// 获取所有用户信息
+	public function getAll($where = '' , $order = 'id  ASC', $limit='') {
+		return $this->where($where)->order($order)->limit($limit)->select();
+	}
+
+	// 获取单个用户信息
+	public function get($where = '',$field = '*') {
+		return $this->field($field)->where($where)->find();
+	}
+
+	// 删除用户
+	public function del($where) {
+		if($where){
+			return $this->where($where)->delete();
+		}else{
+			return false;
+		}
+	}
+
+	// 更新用户
+	public function update($data) {
+		if($data){
+			return $this->save($data);
+		}else{
+			return false;
+		}
+	}
+
+}
